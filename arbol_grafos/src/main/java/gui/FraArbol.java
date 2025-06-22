8531fc8781d5001150d128b673ac937d314165d3
Ariana <ariana.nikol30@gmail.com>
2025-06-22 03:31:38 -0500

    Agregue mi nombre Ariana

--- arbol_grafos/src/main/java/gui/FraArbol.java
+++ arbol_grafos/src/main/java/gui/FraArbol.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gui;
 
-/**
- *
- * @author Enrique
- */
-public class FraArbol {
-    
+import dto.Usuario;
+import dao.arbol;
+
+public class FraArbol extends arbol {
+    public static void main(String[] args) {
+        FraArbol arbol = new FraArbol();
+        
+        // Usamos los métodos heredados directamente
+        arbol.agregar(new Usuario("Carlos", "123456789"));  
+        arbol.agregar(new Usuario("Ana", "987654321"));
+        arbol.agregar(new Usuario("Zoe", "555555555"));
+        arbol.agregar(new Usuario("Beto", "111111111"));
+        
+        arbol.imprimirInOrden();
+    }
 }
