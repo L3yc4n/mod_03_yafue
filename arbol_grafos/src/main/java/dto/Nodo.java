bd91b037fa811487d7e13d4cd15682afa0f41df1
Ariana <ariana.nikol30@gmail.com>
2025-06-22 00:47:12 -0500

    Agregue mi nombre Ariana

--- arbol_grafos/src/main/java/dto/Nodo.java
+++ arbol_grafos/src/main/java/dto/Nodo.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dto;
 
-/**
- *
- * @author Enrique
- */
 public class Nodo {
-    
+   private int valor;
+   private  Nodo derecha;
+   private Nodo izquierda;
+   private int contador;
+   private int altura;
+   
+    public Nodo(int valor){
+        this.valor=valor;
+        derecha=null;
+        izquierda=null;
+        contador=1;
+        altura=1;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void incrementarContador() {
+        this.contador++;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
 }
