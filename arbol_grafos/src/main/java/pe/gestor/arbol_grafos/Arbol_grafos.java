fedbd6eef77c420d908f523483a0389561f97797
Enrique <Enrique@192.168.0.102>
2025-06-21 23:37:37 -0500

    avance

---
+++ arbol_grafos/src/main/java/pe/gestor/arbol_grafos/Arbol_grafos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.gestor.arbol_grafos;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Arbol_grafos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
